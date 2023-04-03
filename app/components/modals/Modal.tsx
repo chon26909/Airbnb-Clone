f44d441ada58c92d4a546fddae9e0fd463447726
HiChon <59905927+chon26909@users.noreply.github.com>
2023-04-04 00:26:18 +0700

    update

---
+++ app/components/modals/Modal.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { ReactElement, FC, useState, useEffect, useCallback } from "react";
+
+interface ModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: ReactElement;
+    footer?: ReactElement;
+    actionLabel?: string;
+    disabled?: boolean;
+    secondaryAction: () => void;
+    secondaryLabel?: string;
+}
+
+const Modal: FC<ModalProps> = (props) => {
+    const { isOpen, onClose, onSubmit, title, body, footer, actionLabel, disabled, secondaryAction, secondaryLabel } = props;
+    // const [showModal, setShowModal] = useState(false);
+
+    // useEffect(() => {
+    //     setShowModal(isOpen);
+    // }, [isOpen]);
+
+    // const handleClose = useCallback(() => {
+    //     if (disabled) return;
+
+    //     setShowModal(false);
+    //     setTimeout(() => {
+    //         onClose();
+    //     }, 300);
+    // }, [disabled, onClose]);
+
+    // const handleSubmit = useCallback(() => {
+    //     if (disabled) return;
+
+    //     onSubmit();
+    // }, [disabled, onSubmit]);
+
+    // const handleSecoundaryAction = useCallback(() => {
+    //     if (disabled || !secondaryAction) return;
+
+    //     secondaryAction();
+    // }, [disabled, secondaryAction]);
+
+    // if (!isOpen) return null;
+
+    return (
+        <>
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70"></div>
+        </>
+    );
+};
+
+export default Modal;
