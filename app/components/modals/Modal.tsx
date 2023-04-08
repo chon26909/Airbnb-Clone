105844ee2b239415eaec6c642bd549b7e14f0b95
chonlasit.s <chonlasit.s@arise.tech>
2023-04-08 20:50:01 +0700

    u

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { ReactElement, FC, useState, useCallback, useEffect } from "react";
-
+import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
 interface ModalProps {
     isOpen: boolean;
     onClose: () => void;
@@ -9,14 +10,14 @@ interface ModalProps {
     title?: string;
     body?: ReactElement;
     footer?: ReactElement;
-    actionLabel?: string;
+    actionLabel: string;
     disabled?: boolean;
     secondaryAction?: () => void;
-    secondaryLabel?: string;
+    secondaryActionLabel?: string;
 }
 
 const Modal: FC<ModalProps> = (props) => {
-    const { isOpen, onClose, onSubmit, title, body, footer, actionLabel, disabled, secondaryAction, secondaryLabel } = props;
+    const { isOpen, onClose, onSubmit, title, body, footer, actionLabel, disabled, secondaryAction, secondaryActionLabel } = props;
     const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
@@ -58,13 +59,39 @@ const Modal: FC<ModalProps> = (props) => {
                     className="relative mx-auto my-6 h-full w-full 
                 md:h-auto md:w-4/6 lg:h-auto lg:w-3/6 xl:w-3/6"
                 >
-                    <div className={`
+                    <div
+                        className={`
+                        h-full
                         transition
                         duration-300
-                        h-full
-                        ${}
-                    `}>
-
+                        ${showModal ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"}
+                    `}
+                    >
+                        <div
+                            className="translate
+                            relative flex h-full w-full 
+                            flex-col rounded-lg border-0 bg-white 
+                            shadow-lg outline-none focus:outline-none md:h-auto lg:h-auto"
+                        >
+                            {/* header */}
+                            <div className="relative flex items-center justify-center rounded-t border-b-[1px] p-6">
+                                <button onClick={handleClose} className="absolute right-9 border-0 p-1 transition hover:opacity-70">
+                                    <IoMdClose size={18} />
+                                </button>
+                                <div className="text-lg font-semibold">{title}</div>
+                            </div>
+                            {/* body */}
+                            <div className="relative flex-auto p-6">{body}</div>
+                            {/* footer */}
+                            <div className="flex flex-col gap-2 p-6">
+                                <div className="flex w-full flex-row items-center gap-4">
+                                    {secondaryAction && secondaryActionLabel && (
+                                        <Button disabled={disabled} label={secondaryActionLabel} onClick={handleSecoundaryAction} outline />
+                                    )}
+                                    <Button disabled={disabled} label={actionLabel} onClick={handleSubmit} />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
