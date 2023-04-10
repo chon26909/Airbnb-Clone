1e26e0fd6abb1f4bca58756cee11b19d338d9a5f
chonlasit.s <chonlasit.s@arise.tech>
2023-04-10 19:17:19 +0700

    create registet modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -5,6 +5,9 @@ import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "../../hooks/useRegisterModal";
+import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -35,7 +38,26 @@ const RegisterModal = () => {
         }
     };
 
-    return <div>RegisterModal</div>;
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Heading title="Welcome to Airbnb" subtitle="create an account" />
+            <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
+            <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
+            <Input id="password" label="Password" type="password" disabled={isLoading} register={register} errors={errors} required />
+        </div>
+    );
+
+    return (
+        <Modal
+            disabled={isLoading}
+            isOpen={registerModal.isOpen}
+            title="Register"
+            actionLabel="Continue"
+            onClose={registerModal.onClose}
+            onSubmit={handleSubmit(onSubmit)}
+            body={bodyContent}
+        />
+    );
 };
 
 export default RegisterModal;
