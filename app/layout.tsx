aac9687e2cb361c89c1d76a1ffd399097a8fad2c
chon26909 <59905927+chon26909@users.noreply.github.com>
2023-04-10 23:02:52 +0700

    change layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import ClientOnly from "./components/ClientOnly";
 import Modal from "./components/modals/Modal";
 import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
+import ToasterProvider from "./components/providers/ToasterProvider";
 import "./globals.css";
 
 export const metadata = {
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en">
             <body>
                 <ClientOnly>
+                    <ToasterProvider />
                     <RegisterModal />
                     <Navbar />
                 </ClientOnly>
