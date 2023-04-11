1d6e06ba80bf2a519126e76d752ae17f6ad9a04b
chon26909 <59905927+chon26909@users.noreply.github.com>
2023-04-11 23:20:38 +0700

    create register route

---
+++ app/api/register/route.ts
@@ -0,0 +1,18 @@
+import prisma from '../../libs/prismadb';
+import { NextResponse } from 'next/server'
+import bcrypt from 'bcrypt';
+
+export const POST = async (req: Request) => {
+  const body = await req.json();
+  const { email, name, password } = body;
+
+  const hashedPassword = await bcrypt.hash(password, 12)
+
+  const user = await prisma.user.create({
+    data: {
+      email, name, hashedPassword
+    }
+  })
+
+  return NextResponse.json(user)
+}
