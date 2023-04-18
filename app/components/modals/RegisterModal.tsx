0afe77f61567ace7c750a3ea8004b2e115c8b08a
chon26909 <59905927+chon26909@users.noreply.github.com>
2023-04-18 19:15:01 +0700

    update register modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,18 +1,24 @@
 "use client";
-import React, { useState, useCallback } from "react";
+
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import useRegisterModal from "../../hooks/useRegisterModal";
-import Modal from "./Modal";
-import Heading from "../Heading";
-import Input from "../inputs/Input";
+import { useCallback, useState } from "react";
 import { toast } from "react-hot-toast";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
+import Modal from "./Modal";
+import Input from "../inputs/Input";
+import Heading from "../Heading";
 import Button from "../Button";
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
     const [isLoading, setIsLoading] = useState(false);
 
     const {
@@ -27,23 +33,32 @@ const RegisterModal = () => {
         }
     });
 
-    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true);
 
-        try {
-            await axios.post("/api/register", data);
-            registerModal.onClose();
-            setIsLoading(false);
-        } catch (error) {
-            setIsLoading(false);
-            console.log(error);
-            toast.error("Something went wrong");
-        }
+        axios
+            .post("/api/register", data)
+            .then(() => {
+                toast.success("Registered!");
+                registerModal.onClose();
+                loginModal.onOpen();
+            })
+            .catch((error) => {
+                toast.error(error);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
     };
+
+    const onToggle = useCallback(() => {
+        registerModal.onClose();
+        loginModal.onOpen();
+    }, [registerModal, loginModal]);
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
-            <Heading title="Welcome to Airbnb" subtitle="create an account" />
+            <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
             <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
             <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
             <Input id="password" label="Password" type="password" disabled={isLoading} register={register} errors={errors} required />
@@ -53,15 +68,16 @@ const RegisterModal = () => {
     const footerContent = (
         <div className="mt-3 flex flex-col gap-4">
             <hr />
-            <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}} />
-            <Button outline label="continue with Github" icon={AiFillGithub} onClick={() => {}} />
-            <div className="mt-4 text-center font-light text-neutral-500">
-                <div className="flex flex-row items-center justify-center gap-2">
-                    <div>Already have an account?</div>
-                    <div onClick={registerModal.onClose} className="cursor-pointer text-neutral-800 hover:underline">
+            <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => signIn("google")} />
+            <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => signIn("github")} />
+            <div className="font-lighttext-neutral-500 mt-4 text-center">
+                <p>
+                    Already have an account?
+                    <span onClick={onToggle} className="cursor-pointertext-neutral-800 hover:underline">
+                        {" "}
                         Log in
-                    </div>
-                </div>
+                    </span>
+                </p>
             </div>
         </div>
     );
