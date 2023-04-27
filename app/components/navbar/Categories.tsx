957a47d3282e091bfa65c3f725a953eabe09a394
chon26909 <59905927+chon26909@users.noreply.github.com>
2023-04-27 19:42:50 +0700

    category

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -3,9 +3,11 @@ import React from "react";
 import Container from "../Container";
 
 import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
-import { GiWindmill, GiIsland, GiBoatFishing } from "react-icons/gi";
+import { GiWindmill, GiIsland, GiBoatFishing, GiCastle, GiForestCamp, GiCaveEntrance, GiBarn } from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
+import { BsSnow } from "react-icons/bs";
 import { FaSkiing } from "react-icons/fa";
+import { IoDiamond } from "react-icons/io5";
 
 import CategoryBox from "../CategoryBox";
 import { usePathname, useSearchParams } from "next/navigation";
@@ -47,7 +49,44 @@ export const categories = [
         description: "This property is close to a lake!"
     },
     {
-        label: "Skiing"
+        label: "Skiing",
+        icon: FaSkiing,
+        description: "This property has skiing activities"
+    },
+    {
+        label: "Castles",
+        icon: GiCastle,
+        description: "This property is in a castles"
+    },
+    {
+        label: "Camping",
+        icon: GiForestCamp,
+        description: "This property has camping activities"
+    },
+    {
+        label: "Arctic",
+        icon: BsSnow,
+        description: "This property has camping activities"
+    },
+    {
+        label: "Cave",
+        icon: GiCaveEntrance,
+        description: "This property is in a cave!"
+    },
+    {
+        label: "Desert",
+        icon: GiBarn,
+        description: "This property is in the desert!"
+    },
+    {
+        label: "Barns",
+        icon: GiBarn,
+        description: "This property is in the barn!"
+    },
+    {
+        label: "Lux",
+        icon: IoDiamond,
+        description: "This property is luxurious!"
     }
 ];
 
